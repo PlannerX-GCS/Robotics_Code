79206768e896ec00778f231a14617a5ae9aba95d
PlannerX-GCS <plannerxgcs@gmail.com>
2025-05-09 22:10:48 +0530

    Update main.py

--- Distance Meter (Pro)/main.py
+++ Distance Meter (Pro)/main.py
@@ -1,7 +1,7 @@
 from time import sleep
 from ROBOT_ACTIONS import obstacle_distance
 
-flicker_lcd(1, 0.1) #Initialises your LCD Screen with an flickering animation (First value (1) is total flicker time, and Second Value (0.1) is time interval at which flickering happens)
+flicker_lcd(1, 0.1) 
 
 write_on_lcd("Initialising", 0, 0 )
 sleep(2)
@@ -13,10 +13,10 @@ while True:
     data_stream("USB")
     distance = obstacle_distance()
     if distance == -1:
-        te_on_lcd(prev_distance, 0, 0 )
+        write_on_lcd(prev_distance, 0, 0 )
     else:
         prev_distance = distance
-        te_on_lcd(distance, 0, 0 )
+        write_on_lcd(distance, 0, 0 )
     sleep(0.2)
     lcd.clear()
     
