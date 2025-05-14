129414a9a3d4699518ba9f3ab4bd417f4dd2d4d7
PlannerX-GCS <plannerxgcs@gmail.com>
2025-05-14 17:57:08 +0530

    Update main.py

--- Data Streamer/main.py
+++ Data Streamer/main.py
@@ -1,14 +1,6 @@
 from time import sleep
 from ROBOT_ACTIONS import *
 
-flicker_lcd(1, 0.1) 
-
-write_on_lcd("Initialising", 0, 0 )
-sleep(2)
-lcd.clear()
-
-prev_distance = 0
-
 while True:
     data_stream("USB")
     sleep(0.0002)
