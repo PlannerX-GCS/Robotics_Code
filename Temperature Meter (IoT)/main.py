d8cb56a5344c178cde79a14cbdf1652d4cc2699d
PlannerX-GCS <plannerxgcs@gmail.com>
2025-05-14 18:11:49 +0530

    Add files via upload

---
+++ Temperature Meter (IoT)/main.py
@@ -0,0 +1,21 @@
+from time import sleep
+from ROBOT_ACTIONS import *
+from INTERNAL_SENSOR_STREAM import *
+
+flicker_lcd(1, 0.1) 
+
+write_on_lcd("Starting...", 0, 0 )
+sleep(2)
+clean_lcd()
+
+
+while True:
+    data_stream("USB")
+    ax, ay, az, gx, gy, gz, error_code, pitch, roll, tempC, pres_hPa, altitude, battery_voltage = internal_sensor_datas()
+
+    write_on_lcd("Temperature", 0, 0)
+    write_on_lcd(tempC, 1, 0)
+
+    sleep(0.5)
+    clean_lcd()
+    
