6f9118ba1d1c886dedd503ca2a96e86d1efeb9fe
PlannerX-GCS <plannerxgcs@gmail.com>
2025-05-14 17:51:53 +0530

    Update main.py

--- Data Streamer/main.py
+++ Data Streamer/main.py
@@ -11,12 +11,5 @@ prev_distance = 0
 
 while True:
     data_stream("USB")
-    distance = obstacle_distance()
-    if distance == -1:
-        write_on_lcd(prev_distance, 0, 0 )
-    else:
-        prev_distance = distance
-        write_on_lcd(distance, 0, 0 )
-    sleep(0.002)
-    lcd.clear()
+    sleep(0.0002)
     
